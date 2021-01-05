439117b5d038e8d216321ce53332251299dea869
Jeff Inman <jeffery.inman@hotmail.com>
2021-01-04 18:23:10 -0800

    created smiley face

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 1800
+canvas.height = 700
 
 class Player1 {
     constructor(x, y, radius, color) {
@@ -14,7 +14,7 @@ class Player1 {
 
     draw() {
         c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
         c.fillStyle = this.color
         c.fill()
     }
@@ -59,18 +59,24 @@ class Player4 {
 
     draw() {
         c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 1, false)
         c.fillStyle = this.color
         c.fill()
     }
 }
 
-const player1 = new Player1 (500, 400, 300, 'blue')
-const player2 = new Player2 (300, 100, 50, 'white')
-const player3 = new Player3 (200, 100, 50, 'white')
-const player4 = new Player4 (300, 100, 50, 'white')
+const player1 = new Player1 (canvas.width / 2, canvas.height / 2, 300, 'blue')
+const player2 = new Player2 (canvas.width - 750, 200, 100, 'white')
+const player3 = new Player3 (canvas.width - 1025, 200, 100, 'white')
+const player4 = new Player4 (canvas.width / 2, 400, 150, 'green')
 
 player1.draw()
 player2.draw()
 player3.draw()
 player4.draw()
+
+addEventListener('click',(event) => {
+    console.log(event.clientX)
+    const projectile = new Projectile (
+    )
+})
