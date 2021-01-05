81798fb98cac50655f75051ed677efab5286895b
Jeff Inman <jeffery.inman@hotmail.com>
2021-01-04 18:45:07 -0800

    created wolly willy, lol

--- test.js
+++ test.js
@@ -65,18 +65,36 @@ class Player4 {
     }
 }
 
-const player1 = new Player1 (canvas.width / 2, canvas.height / 2, 300, 'blue')
-const player2 = new Player2 (canvas.width - 750, 200, 100, 'white')
-const player3 = new Player3 (canvas.width - 1025, 200, 100, 'white')
-const player4 = new Player4 (canvas.width / 2, 400, 150, 'green')
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
 
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+const player1 = new Player1 (canvas.width / 2, canvas.height / 2, 300, 'blue')
+const player2 = new Player2 (canvas.width - 750, 200, 100, 'yellow')
+const player3 = new Player3 (canvas.width - 1025, 200, 100, 'yellow')
+const player4 = new Player4 (canvas.width / 2, 400, 150, 'green')
 player1.draw()
 player2.draw()
 player3.draw()
 player4.draw()
 
+
 addEventListener('click',(event) => {
-    console.log(event.clientX)
-    const projectile = new Projectile (
-    )
+    const projectile = new Projectile (event.clientX, event.clientY, 5, 'red', null)
+    projectile.draw
 })
+
+
